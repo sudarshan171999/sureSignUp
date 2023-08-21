13b4b5d4f33947028dc1a0218598c4bf14fc7d21
sudarshan171999 <142786049+sudarshan171999@users.noreply.github.com>
2023-08-21 14:05:04 +0530

    Add files via upload

---
+++ C01.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+// int main()
+// {
+//     int i = -3;
+//     int k = i % 2;
+//     printf("%d\n", k);
+// }
+
+// int main()
+// {
+//     int x = 1, y = 0, z = 5;
+//     int a = x && y && z++;
+//     printf("%d\n", z);
+// }
+
+// int main()
+// {
+//     int a = 10;
+//     printf("%d", a);
+//     myshow(&a);
+//     printf("%d", a);
+// }
+// void myshow(int *k)
+// {
+//     *k = 20;
+// }
+
+// int main()
+// {
+//     int a=0,b=5,c=8;
+//     if(!(a==9))
+//     {
+//         printf("Bear\n");
+//     }
+//     else{
+//         printf("Elephant\n");
+//     }
+//     printf("Fox");
+//     return 0;
+// }
+
+// int main()
+// {
+//     int x = 5.3 % 2;
+//     printf("Value: %d", x);
+// }
+int main()
+{
+    int a, b, n;
+    scanf(&a, &b, &n);
+    printf("%d %d %d", a, b, n);
+}
